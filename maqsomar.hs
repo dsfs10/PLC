f75d330c7f48aa9b695eb1d86d6aff0e537e7b80
Daniel Silvestre de França e Souza <136166261+dsfs10@users.noreply.github.com>
2025-05-20 16:46:41 -0300

    Create maqsomar.hs

---
+++ maqsomar.hs
@@ -0,0 +1,11 @@
+maquinaSomar :: [Int] -> [Int]
+maquinaSomar [0,0] = []
+--maquinaSomar (0:0:as) = []
+maquinaSomar (a:(b:as)) | a /= 0 && b == 0 = [a]              
+                        | a == 0 && b /= 0 = maquinaSomar (b:as)
+                        | a == 0 && b == 0 = maquinaSomar (a:(b:[]))
+                        | otherwise = (a+b+head(maquinaSomar(as)):[])
+
+main = do
+       lista <- getLine
+       print $ maquinaSomar (read lista :: [Int])
