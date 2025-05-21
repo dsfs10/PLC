0fa864b63edf6ba0e4ea02960ff587d57dc85e7e
Daniel Silvestre de França e Souza <136166261+dsfs10@users.noreply.github.com>
2025-05-20 23:48:36 -0300

    Update maqsomar.hs

--- maqsomar.hs
+++ maqsomar.hs
@@ -11,11 +11,38 @@ main = do
        print $ maquinaSomar (read lista :: [Int])
 
 ------------------------------------------------------------
-V 2.0
+v 2.0
 maquinaSomar :: [Int] -> [Int]
 maquinaSomar (a:(b:as)) | a /= 0 && b == 0 = [a]              
                         | a == 0 && b == 0 = []
                         | b /= 0 = maquinaSomar (a+b:as)
+
+main = do
+       lista <- getLine
+       print $ maquinaSomar (read lista :: [Int])
+-------------------------------------------------------------
+v 2.1
+maquinaSomar :: [Int] -> [Int]
+maquinaSomar [] = []
+maquinaSomar [0] = []
+maquinaSomar (a:(b:as)) | b == 0 && a == 0 = []
+                        | b == 0 && head(as) == 0 = [a]
+                        | b == 0 && head(as) /= 0 = maquinaSomar (as)
+                        | b /= 0 && head(as) == 0 = maquinaSomar (b:maquinaSomar(as))
+                        | a /= 0 = maquinaSomar (b:as)
+                        | otherwise = maquinaSomar (b+head(as):as)
+
+main = do
+       lista <- getLine
+       print $ maquinaSomar (read lista :: [Int])
+-------------------------------------------------------------
+v 2.2 
+maquinaSomar :: [Int] -> [Int]
+maquinaSomar [] = []
+maquinaSomar [0] = []
+maquinaSomar (a:as) | a == 0 && head(as) == 0 = [a]
+                    | a == 0 = maquinaSomar(a+head(as):as)
+                    | otherwise = maquinaSomar (as)
 
 main = do
        lista <- getLine
