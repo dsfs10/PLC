20022c80f2b780468642cca5b3e5442eb9624a82
Daniel Silvestre de França e Souza <136166261+dsfs10@users.noreply.github.com>
2025-07-09 11:14:53 -0300

    Delete aula1.hs

--- aula1.hs
+++
@@ -1,9 +0,0 @@
-square :: Int -> Int
-square x = x * x
-
-allEqual :: Int -> Int -> Int -> Bool
-allEqual n m p = (n == m) && (m == p)
-
-maxi :: Int -> Int -> Int
-maxi n m | n >= m    = n
-         | otherwise = m
